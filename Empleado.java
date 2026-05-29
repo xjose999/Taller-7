43ad50203fabe050db969bdd323cc172b8f31993
xjose999 <josejoestar80@gmail.com>
2026-05-29 15:46:51 -0500

    Ejericio 1

--- Empleado.java
+++ Empleado.java
@@ -1,2 +1,21 @@
-public class Empleado {
+class Empleado {
+    public String nombre;
+    private double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        setSalario(salario);
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario >= 0) {
+            this.salario = salario;
+        } else {
+            System.out.println("El salario no puede ser negativo.");
+        }
+    }
 }
