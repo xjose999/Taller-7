71aa2446579bb14848b28e1e9633362833011bf9
xjose999 <josejoestar80@gmail.com>
2026-05-29 16:06:11 -0500

    Ejercicio 3

---
+++ Utilidades.java
@@ -0,0 +1,21 @@
+class Utilidades {
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: División por cero.");
+            return 0;
+        }
+        return a / b;
+    }
+}
